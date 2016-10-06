a88eb51952270bf07e31f778226fdf9537d15ece
Stephan Yu <Stephan.yu@gmail.com>
2016-10-06 18:38:44 +0100

    Move fetch_video service class to lib folder
    keep classes that do not require Rails in separate lib folder.

---
+++ lib/fetch_video_service.rb
@@ -0,0 +1,13 @@
+class FetchVideoService
+  include HTTParty
+  base_uri 'https://www.googleapis.com/youtube'
+  format :json
+  
+  def initialize(video_uid, api_key, fields = nil, part = nil)
+    @options = { query: {id: video_uid, key: api_key, fields: fields, part: part}.compact }
+  end
+
+  def videos
+    self.class.get("/v3/videos", @options)["items"]
+  end
+end
