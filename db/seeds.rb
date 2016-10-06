4a54c4533361b1810945fa98d90aa9252b765377
Stephan Yu <Stephan.yu@gmail.com>
2016-10-06 21:08:50 +0100

    Fix seeds.rb file
    correct associations between models by linking to model.id's rather than model.uid's.
    nest artist and city conditional blocks inside of song conditional block so variables are
    declared correctly. implement a find_by uid or create logic to initialize database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,33 +4,38 @@ api_key   = ENV["youtube_api_key"]
 fields    = "items(id,snippet(thumbnails))"
 part      = "snippet"
 
-binding.pry
-videos.map do |video|
+videos.all.map do |video|
   video_uid         = video['video_uid']
-  youtube_video     = FetchVideoService.new(video_uid, api_key, fields, part).videos.first
-  thumbnail_url     = youtube_video["snippet"]["thumbnails"]["medium"]["url"]
+  youtube_video     = FetchVideoService.new(video_uid, api_key, fields, part).videos.first 
+  thumbnail_url     = youtube_video["snippet"]["thumbnails"]["medium"]["url"] if youtube_video.present?
+  
 
   if video['song'].present?
-    song_uid        = video['song']['id']
-    song_artist_uid = video['song']['artist_id']
-    song_title      = video['song']['title']
-    song_slug       = video['song']['cached_slug']
-    song_city_uid   = video['song']['city_id']
+    song   = video['song']
+    artist = song['artist']
+    city   = song['city']
+
+    song_uid        = song['id']
+    song_artist_uid = song['artist_id']
+    song_title      = song['title']
+    song_slug       = song['cached_slug']
+    song_city_uid   = song['city_id']
+
+    if artist.present?
+      artist_title  = artist['title']
+      artist_slug   = artist['cached_slug']
+    end
+
+    if city.present?
+      city_title    = city['title']
+      city_slug     = city['cached_slug']
+    end
   end
   
-  if video['artist'].present?
-    artist_title    = video['artist']['title']
-    artist_slug     = video['artist']['cached_slug']
-  end
-
-  if video['city'].present?
-    city_title      = video['city']['title']
-    city_slug       = video['city']['cached_slug']
-  end
 
   artist = Artist.find_by(artist_uid: song_artist_uid)
   if artist.nil?
-    Artist.create(
+    artist = Artist.create(
       artist_uid:  song_artist_uid, 
       title:       artist_title, 
       cached_slug: artist_slug)
@@ -38,21 +43,27 @@ videos.map do |video|
 
   city = City.find_by(city_uid: song_city_uid)
   if city.nil?
-    City.create(
+    city = City.create(
       city_uid:    song_city_uid, 
       title:       city_title, 
       cached_slug: city_slug)
   end
 
-  Song.create(
-    song_uid:    song_uid, 
-    artist_id:   song_artist_uid, 
-    title:       song_title, 
-    cached_slug: song_slug, 
-    city_id:     song_city_uid)
+  song = Song.find_by(song_uid: song_uid)
+  if song.nil?
+    song = Song.create(
+      song_uid:    song_uid, 
+      artist_id:   artist.id, 
+      title:       song_title, 
+      cached_slug: song_slug, 
+      city_id:     city.id)
+  end
 
-  Video.create(
-    video_uid:   video_uid, 
-    thumb_url:   thumbnail_url, 
-    song_id:     song_uid)
+  video = Video.find_by(video_uid: video_uid)
+  if video.nil?
+    Video.create(
+      video_uid:   video_uid, 
+      thumb_url:   thumbnail_url, 
+      song_id:     song.id)
+  end
 end
