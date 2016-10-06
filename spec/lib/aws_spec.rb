a4bc475c4c576f3a1236167fdcd4055d691cd03c
Stephan Yu <Stephan.yu@gmail.com>
2016-10-06 21:13:10 +0100

    Rename spec folders and rearrange files
    replace services folder with lib folder as aws and fetch_video classes now sit in lib folder.
    rename fetch videos spec file following correct naming conventions.

---
+++ spec/lib/aws_spec.rb
@@ -0,0 +1,15 @@
+require 'aws'
+
+describe 'AWS' do 
+  context 'JSON file' do 
+    it 'successfully downloads a file' do 
+      #create JSON file
+      #AWS.download JSON file
+      #expect(response).to be a JSON file
+    end
+
+    it 'returns a file with a specific property' do 
+      
+    end
+  end
+end
--- spec/services/aws_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'aws'
-
-describe 'AWS' do 
-  it 'successfully downloads a json file' do 
-    aws_service = double("AWS", :download => {"key": "value"} )
-   
-    expect(aws_service.download).to eq({"key": "value"})
-  end
-end
