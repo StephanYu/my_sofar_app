663c9432ff09874471fceea3bee597a169ff8d32
Stephan Yu <Stephan.yu@gmail.com>
2016-10-13 07:34:21 +0100

    Fix indentation of  private methods.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -16,42 +16,43 @@ class VideosController < ApplicationController
   end
 
   private
-    def fetch_video(video_uid, api_key)
-      fields = 'items(id,snippet(publishedAt,description,title),statistics)'
-      part = 'snippet,statistics'
-      video = FetchVideoService.new(video_uid, api_key, fields, part).videos.first
 
-      video
-    end
+  def fetch_video(video_uid, api_key)
+    fields = 'items(id,snippet(publishedAt,description,title),statistics)'
+    part = 'snippet,statistics'
+    video = FetchVideoService.new(video_uid, api_key, fields, part).videos.first
 
-    def create_video_profile(video)
-      result = fetch_video(video.video_uid, @api_key)
-      snippet = result['snippet']
-      statistics = result['statistics']
-      
-      set_video_snippet(snippet)
-      set_video_statistics(statistics)
-    end
+    video
+  end
 
-    def set_video_credentials
-      @api_key = ENV['youtube_api_key']
-    end
+  def create_video_profile(video)
+    result = fetch_video(video.video_uid, @api_key)
+    snippet = result['snippet']
+    statistics = result['statistics']
+    
+    set_video_snippet(snippet)
+    set_video_statistics(statistics)
+  end
 
-    def set_video_statistics(stats)
-      @video_views = stats['viewCount']
-      @video_likes = stats['likeCount'] 
-      @video_dislikes = stats['dislikeCount'] 
-      @video_favorites = stats['favoriteCount'] 
-      @video_comments = stats['commentCount'] 
-    end
+  def set_video_credentials
+    @api_key = ENV['youtube_api_key']
+  end
 
-    def set_video_snippet(stats)
-      @video_description = stats['description']
-      @video_published_at = stats['publishedAt']
-      @video_title = stats['title']
-    end
+  def set_video_statistics(stats)
+    @video_views = stats['viewCount']
+    @video_likes = stats['likeCount'] 
+    @video_dislikes = stats['dislikeCount'] 
+    @video_favorites = stats['favoriteCount'] 
+    @video_comments = stats['commentCount'] 
+  end
 
-    def set_video
-      @video = Song.find(params[:id]).video
-    end
+  def set_video_snippet(stats)
+    @video_description = stats['description']
+    @video_published_at = stats['publishedAt']
+    @video_title = stats['title']
+  end
+
+  def set_video
+    @video = Song.find(params[:id]).video
+  end
 end
--- app/services/video_importer_service.rb
+++ app/services/video_importer_service.rb
@@ -1,5 +1,10 @@
 class VideoImporterService
-  
+  attr_reader :videos
+
+  def initialize(videos)
+    @videos = videos
+  end
+
   def process(videos, api_key, fields, part)
     videos.all.map do |video|
       video_uid      = video['video_uid']
@@ -14,58 +19,57 @@ class VideoImporterService
   end
 
   private
-    def get_thumbnail(video)
-      video['snippet']['thumbnails']['medium']['url'] if video.present?
-    end
+  
+  def get_thumbnail(video)
+    video['snippet']['thumbnails']['medium']['url'] if video.present?
+  end
 
-    def build_song(song_hash)
-      song = Song.find_by(song_uid: song_hash['id'])
-      return song unless song.nil?
-      Song.new(
-          song_uid:    song_uid, 
-          artist:   build_artist(song_hash['artist']), 
-          title:       song_title, 
-          cached_slug: song_slug, 
-          city:     build_city(song_hash['city']))
+  def build_song(song_hash)
+    song = Song.find_by(song_uid: song_hash['id'])
+    return song unless song.nil?
+    Song.new(
+        song_uid:    song_uid, 
+        artist:   build_artist(song_hash['artist']), 
+        title:       song_title, 
+        cached_slug: song_slug, 
+        city:     build_city(song_hash['city']))
+    end
+  end
+
+  def build_artist(uid)
+
+  end
+
+  def build_city(uid)
+
+  end
+
+  def create_video(video)
+    # create Video object
+    video_obj = Video.new
+    video_obj.song = build_song(video['song'])
+    # Video.save
+
+    if video['song'].present?
+      song   = video['song']
+      artist = song['artist']
+      city   = song['city']
+
+      song_uid        = song['id']
+      song_artist_uid = song['artist_id']
+      song_title      = song['title']
+      song_slug       = song['cached_slug']
+      song_city_uid   = song['city_id']
+
+      if artist.present?
+        artist_title  = artist['title']
+        artist_slug   = artist['cached_slug']
+      end
+
+      if city.present?
+        city_title    = city['title']
+        city_slug     = city['cached_slug']
       end
     end
-
-    def build_artist(uid)
-
-    end
-
-    def build_city(uid)
-
-    end
-
-    def create_video(video)
-      # create Video object
-      video_obj = Video.new
-      video_obj.song = build_song(video['song'])
-      # Video.save
-
-      if video['song'].present?
-        song   = video['song']
-        artist = song['artist']
-        city   = song['city']
-
-        song_uid        = song['id']
-        song_artist_uid = song['artist_id']
-        song_title      = song['title']
-        song_slug       = song['cached_slug']
-        song_city_uid   = song['city_id']
-
-        if artist.present?
-          artist_title  = artist['title']
-          artist_slug   = artist['cached_slug']
-        end
-
-        if city.present?
-          city_title    = city['title']
-          city_slug     = city['cached_slug']
-        end
-      end
-      # todo: create hash object with all variables set as keys
-      return {}
-    end
+  end
 end
